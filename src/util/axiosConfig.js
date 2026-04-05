912caf023d4c4894f135d21ec9aff68db2052905
Osama Farag <ofarag115@gmail.com>
2026-04-05 15:34:45 -0400

    created apiEndpoints and add API base URL and endpoints constants

--- src/util/axiosConfig.js
+++ src/util/axiosConfig.js
@@ -1,7 +1,8 @@
 import axios from "axios";
+import { BASE_URL } from "./ApiEndpoints";
 
 const axiosConfig = axios.create({
-    baseURL: "https://money-manager-application-gbib.onrender.com/api/v1.0",
+    baseURL: BASE_URL,
     headers: {
         "Content-Type": "application/json",
         Accept: "application/json"
