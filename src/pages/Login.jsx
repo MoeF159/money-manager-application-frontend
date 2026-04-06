2be493a09d55e119d6ede13e126a72fc01d4702e
Osama Farag <ofarag115@gmail.com>
2026-04-06 13:01:22 -0400

    created the Login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,76 @@
+import { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import {assets} from "../assets/assets.js"
+import Input from "../components/Input.jsx";
+
 const Login = () => {
+
+    const[email, setEmail] = useState("");
+    const[password, setPassword] = useState("");
+    const[error, setError] = useState(null);
+
+    const navigate = useNavigate();
+
+
     return (
-        <div>Login</div>
+        <div className="h-screen w-full relative flex items-center justify-center overflow-hidden">
+            {/* Background image with blur */}
+            <img src={assets.login_bg} alt="Background" className="absolute inset-0 w-full h-full object-cover filter blur-sm"/>
+            
+            <div className="relative z-10 w-full max-w-lg px-6">
+                <div className="bg-white bg-opacity-95 backdrop-blur-sm rounded-lg shaodw-2xl p-8 max-h-[90vh] overflow-y-auto">
+                    <h3 className="text-2xl font-semibold text-black text-center mb-2">
+                        Welcome back to Money Manager
+                    </h3>
+                    <p className="text-sm text-slate-700 text-center mb-8">
+                        Please enter your details to log in
+                    </p>
+
+                    <form onSubmit="" className="space-y-4">
+                        {/* Input Components */}
+                        <Input 
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        label={"Email Address"}
+                        placeholder={"name@example.com"}
+                        type={"text"}
+                        />
+                    
+                        <Input 
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        label={"Password"}
+                        placeholder={"********"}
+                        type={"password"}
+                        />
+
+                        {error && (
+                            <p className="text-red-800 text-sm text-center bg-red-50 p-2 rounded">
+                                {error}
+                            </p>
+                        )}
+                        <button 
+                        className="bg-green-700 text-white w-full py-3 text-lg font-medium rounded-lg" 
+                        type="submit"
+                        >
+                            LOGIN
+                        </button>
+
+                        <p className="text-sm text-slate-800 text-center mt-6">
+                            Dont have an account?{" "}
+                            <Link 
+                            to="/signup"
+                            className="font-medium text-primary underline hover:text-primary-dark transition-colors"
+                            > 
+                                Register!
+                            </Link>
+                        </p>
+
+                    </form>
+                </div>
+            </div>
+
+        </div>
     )
 }
 
