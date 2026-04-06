cb07fb2c67f1cba700bc6090c4cdeca4abd6e7a1
Osama Farag <ofarag115@gmail.com>
2026-04-06 12:42:27 -0400

    imported Eye and EyeOff from lucide and implemented showPassword toggling

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,4 +1,14 @@
+import { Eye, EyeOff } from "lucide-react";
+import { useState } from "react";
+
 const Input = ({label, value, onChange, placeholder, type}) => {
+    
+    const [showPassowrd, setShowPassword] = useState(false);
+
+    const toggleShowPassword = () => {
+        setShowPassword(!showPassowrd)
+    }
+
     return (
         <div className="mb-4">
             <label className="text-[13px] text-slate-800 mb-1">
@@ -7,10 +17,29 @@ const Input = ({label, value, onChange, placeholder, type}) => {
             <div className="relative">
                 <input 
                 className="w-full bg-transparent outline-none border border-gray-300 rounded-md py-2 px-3 pr-10 text-gray-700 leading-tight focus:outline-none focus:border-blue-500"
-                type={type} 
+                type={type === 'password' ? (showPassowrd ? 'text' : 'password') : type} 
                 placeholder={placeholder} 
                 value={value} 
                 onChange={(e) => onChange(e)} />
+
+                {/* Toggle Show password */}
+                {type === 'password' && (
+                    <span className="absolute right-3 top-1/2 -translate-y-1/2 cursor-pointer">
+                        {showPassowrd ? (
+                            <Eye 
+                                size={20}
+                                className="text-green-800"
+                                onClick={toggleShowPassword}
+                            />
+                        ) : (
+                            <EyeOff 
+                                size={20}
+                                className="text-slate-400"
+                                onClick={toggleShowPassword}
+                            />
+                        )}
+                    </span>
+                )}
             </div>
 
         </div>
